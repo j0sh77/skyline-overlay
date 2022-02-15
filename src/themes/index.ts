0faf4372d08e5a04c9e684b14b7fde900eabd64c
Josh <jcjohnson77@gmail.com>
2022-02-14 23:38:31 -0500

    Add customizable simple theme

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -5,6 +5,7 @@ import './default.scss';
 import './horiz.scss';
 import './ikegami.scss';
 import './jround.scss';
+import './simple.scss';
 
 const themes: {[k: string]: Theme} = {
   'default': { text: 'Skyline Overlay', data: { author: authors.DSRKafuU } },
@@ -15,6 +16,14 @@ const themes: {[k: string]: Theme} = {
     data: { author: authors.j0sh77 },
     colors: { background: {r: 0, g: 0, b: 0, a: 0.3} } 
   },
+  simple: {
+    text: 'Simple',
+    colors: {
+      background: {r: 0, g: 0, b: 0, a: 0.3},
+      foreground: {r: 255, g: 255, b: 255, a: 1},
+      border: {r: 255, g: 255, b: 255, a: 1},
+    }
+  }
 };
 
 export default themes;
